b67e413a510846afc711f88348ffd544a2046d3b
Bitrus Dauda <bitrusdauda9118@gail.com>
2025-11-08 14:10:49 -0800

    commit all

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,23 @@
-//Task 2: Directors Interface (Extending Teacher)
+//Task 2: Directors Interface (Extending Teacher
 
+// task_1/js/main.ts
+
+// Base Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allow any additional attributes
+}
+
+// Directors interface that extends Teacher
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+// Example usage
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
@@ -12,3 +26,4 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
+console.log(director1);
